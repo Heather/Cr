b11a3d3bd79cb7f1502563895e9e22fd5b69936c
Heather <heather@cynede.net>
2013-07-22 11:25:46 +0400

    init

---
+++ src/Cr.hs
@@ -0,0 +1,36 @@
+﻿{-# LANGUAGE UnicodeSyntax, CPP #-}
+
+import Data.List
+import Data.Function
+import Data.Char
+import Data.IORef
+import Data.Maybe
+
+import Control.Monad
+import Control.Applicative
+
+import Text.Printf
+
+import Google
+
+version = "0.0.1"
+main = do
+
+    printf "\n  Cr v.%s\n\n" version  {-  Intro  -}
+   
+    ls <- getLastVersionForPlatform "Win"
+    
+    printf "\n ========== "
+    printf "\n -> Getting %s" ls
+
+    getChromium "Win" ls
+    
+    printf "\n -> Done"
+    printf "\n ========== "
+    printf "\n"
+    
+    -- > Wait for keypress (Only for windows)
+#if defined(mingw32_HOST_OS) || defined(__MINGW32__)
+    getChar
+#endif
+    -- >
---
+++ src/Google.hs
@@ -0,0 +1,34 @@
+﻿{-# LANGUAGE UnicodeSyntax #-}
+module Google
+  ( getLastVersionForPlatform
+  , getChromium
+  ) where
+
+import Data.List
+import Data.Function
+import Data.Char
+import Data.IORef
+import Data.Maybe
+
+import Control.Monad
+import Control.Applicative
+
+import Network.HTTP
+import Network.Socket
+import Network.HTTP.Conduit
+
+import qualified Data.ByteString.Lazy as L
+import qualified Codec.Binary.UTF8.String as S
+{------------------------- Last Chromium Version --------------------------------------}
+getLastVersionForPlatform :: [Char] → IO String
+getLastVersionForPlatform s = withSocketsDo
+    $   let url = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/" ++ s ++ "/LAST_CHANGE"
+        in simpleHttp url
+            >>= \bs → return $ S.decode $ L.unpack bs
+{-------------------------  Chromium  --------------------------------------}
+getChromium :: [Char] → [Char] → IO()
+getChromium s v = withSocketsDo
+    $   let url = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?path=" 
+                    ++ s ++ "/" ++ v ++ "/mini-installer.exe"
+        in simpleHttp url >>= L.writeFile "mini-installer.exe"
+{----------------------------------------------------------------------------------------}
