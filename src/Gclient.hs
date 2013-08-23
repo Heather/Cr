bded3a0a1a741bd4a5a7ee961300efa7a1e613b3
Heather <heather@cynede.net>
2013-08-23 14:53:33 +0400

    Run gclient

--- src/Gclient.hs
+++ src/Gclient.hs
@@ -10,6 +10,7 @@ import Codec.Archive.Zip
 
 import System.IO
 import System.Directory
+import System.Process
 
 import Control.Monad
 
@@ -19,6 +20,7 @@ getGitSources :: [Char] → IO()
 getGitSources p = do
     case p of
      "Win" -> do
+        let ddir = "depot_tools"
         doesDirectoryExist "depot_tools" >>= \dirExist → unless dirExist $ do
             let tarball = "depot_tools.zip"
             doesFileExist tarball >>= \fileExist → unless fileExist $ do
@@ -26,5 +28,7 @@ getGitSources p = do
                     >> getDepotTools p
                 dictZipFile <- B.readFile tarball
                 extractFilesFromArchive [] $ toArchive dictZipFile
+            pid <- runCommand $ ddir ++ "gclient"
+            waitForProcess pid >>= \exitWith → putStrLn " -> Done"
      _  -> putStrLn "This platform is not supported yet :("
 {----------------------------------------------------------------------------------------}
