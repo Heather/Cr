def5630814957e63bfed91ed77b4d047b7fdfdca
Heather <heather@cynede.net>
2013-08-23 13:43:41 +0400

    and if directory doesn't exist

--- src/Gclient.hs
+++ src/Gclient.hs
@@ -15,7 +15,8 @@ getGitSources :: [Char] → IO()
 getGitSources p = do
     case p of
      "Win" -> do
-        doesFileExist "depot_tools.zip" >>= \fileExist → unless fileExist
-            $ print " -> Getting Depot Tools" >> getDepotTools p
+        doesDirectoryExist "depot_tools" >>= \dirExist → unless dirExist
+            $ doesFileExist "depot_tools.zip" >>= \fileExist → unless fileExist
+                $ print " -> Getting Depot Tools" >> getDepotTools p
      _  -> putStrLn "This platform is not supported yet :("
 {----------------------------------------------------------------------------------------}
