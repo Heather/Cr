3bce328f1f25fb1f1dea4710a340a5730f783791
Heather <heather@cynede.net>
2013-08-23 14:05:26 +0400

    EXTRACT !

--- src/Gclient.hs
+++ src/Gclient.hs
@@ -6,17 +6,25 @@ module Gclient
 
 import Depot
 
+import Codec.Archive.Zip
+
 import System.IO
 import System.Directory
 
 import Control.Monad
+
+import qualified Data.ByteString.Lazy as B
 {-------------------------------  Depot Tools  -----------------------------------------}
 getGitSources :: [Char] → IO()
 getGitSources p = do
     case p of
      "Win" -> do
-        doesDirectoryExist "depot_tools" >>= \dirExist → unless dirExist
-            $ doesFileExist "depot_tools.zip" >>= \fileExist → unless fileExist
-                $ print " -> Getting Depot Tools" >> getDepotTools p
+        doesDirectoryExist "depot_tools" >>= \dirExist → unless dirExist $ do
+            let tarball = "depot_tools.zip"
+            doesFileExist tarball >>= \fileExist → unless fileExist $ do
+                print " -> Getting Depot Tools" 
+                    >> getDepotTools p
+                dictZipFile <- B.readFile tarball
+                extractFilesFromArchive [] $ toArchive dictZipFile
      _  -> putStrLn "This platform is not supported yet :("
 {----------------------------------------------------------------------------------------}
