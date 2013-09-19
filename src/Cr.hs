8fdbc61d95cb5a2c2db0c0fb040d590f4a494b90
Heather <heather@cynede.net>
2013-09-19 09:01:45 +0400

    fix locking

--- src/Cr.hs
+++ src/Cr.hs
@@ -16,37 +16,19 @@ import Control.Concurrent
 import Control.Monad
 import Control.Applicative
 
-version = "0.1.8"
+version = "0.2.0"
 main = do
-    forkIO oneInstance
-    args <- getArgs
-    let ( actions, nonOpts, msgs ) = getOpt RequireOrder options args
-    opts <- foldl (>>=) (return defaultOptions) actions
-    let Options { optPlatform = platform,
-                optBuild = build } = opts
-    build platform
-    
-oneInstance :: IO ()
-oneInstance = do
     user <- getAppUserDataDirectory "Cr.lock"
     locked <- doesFileExist user
-    if locked
-        then putStrLn "There is already one instance of this program running."
-        else do t <- myThreadId
-                withFile user WriteMode (do_program t)
-                removeFile user
-        
-do_program :: ThreadId -> Handle -> IO ()
-do_program t h = let s = "Locked by thread: " ++ show t
-                 in do  putStrLn s
-                        hPutStr h s
-                        threadDelay 1000000
+    if locked then putStrLn "There is already one instance of this program running."
+              else do t <- myThreadId
+                      withFile user WriteMode (do_program t)
+                      removeFile user
 
 data Options = Options  {
     optPlatform  :: String,
     optBuild :: String → IO()
   }
-
 defaultOptions :: Options
 defaultOptions = Options {
     optPlatform = if | os `elem` ["win32", "mingw32", "cygwin32"] → "Win"
@@ -55,6 +37,16 @@ defaultOptions = Options {
         ,
     optBuild = go "last"
   }
+do_program :: ThreadId -> Handle -> IO ()
+do_program t h = let s = "Locked by thread: " ++ show t
+                 in do  putStrLn s
+                        hPutStr h s
+                        args <- getArgs
+                        let ( actions, nonOpts, msgs ) = getOpt RequireOrder options args
+                        opts <- foldl (>>=) (return defaultOptions) actions
+                        let Options { optPlatform = platform,
+                                    optBuild = build } = opts
+                        build platform
 
 options :: [OptDescr (Options → IO Options)]
 options = [
@@ -67,35 +59,28 @@ options = [
     Option ['b'] ["build"]   (ReqArg getb "STRING") "build number"
   ]
 
-showVersion _ =
-    printf "\n  Cr v.%s\n\n" version
-        >> exitWith ExitSuccess
+showVersion _ = printf "\n  Cr v.%s\n\n" version
+                    >> exitWith ExitSuccess
     
-showHelp _ = do
-    putStrLn $ usageInfo "Usage: Cr [optional things]" options
-    exitWith ExitSuccess
+showHelp _ = do putStrLn $ usageInfo "Usage: Cr [optional things]" options
+                exitWith ExitSuccess
   
 showChromeVersion _ = do
     ls <- getLastVersionForPlatform "Win"
     printf "last: %s\n" ls
         >> exitWith ExitSuccess
 
-getSrc _ =
-    gInit "Win"
-        >> fetch "chromium"
-        >> exitWith ExitSuccess
-        
-getDartium _ =
-    getDart "Win"
-        >> exitWith ExitSuccess
+getSrc _ = gInit "Win" >> fetch "chromium"
+                       >> exitWith ExitSuccess
+
+getDartium _ = getDart "Win" >> exitWith ExitSuccess
 
 getp arg opt = return opt { optPlatform = arg }
 getb arg opt = return opt { optBuild = go arg }
 
-data Config = Config
-  { cr :: String
-  , installed  :: Int
-  } deriving (Read, Show)
+data Config = Config { cr :: String
+                     , installed  :: Int
+    } deriving (Read, Show)
 readConfig :: String → Config
 readConfig = read
 writeConfig :: Config → String
