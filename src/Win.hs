c93cd2eff1446efce05910e4ddc775b411148746
Heather <heather@live.ru>
2014-12-24 11:29:17 +0400

    stdcall -> ccall

--- src/Win.hs
+++ src/Win.hs
@@ -9,7 +9,7 @@ import Graphics.Win32.GDI.Types
 import Foreign.C.String
 import Foreign.Marshal.Array
 
-foreign import stdcall unsafe "SHGetFolderPathW"
+foreign import ccall unsafe "SHGetFolderPathW" --stdcall
     cSHGetFolderPathW :: HWND -> INT -> HANDLE -> DWORD -> CWString -> IO LONG
 
 maxPath = 260
