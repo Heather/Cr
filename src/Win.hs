17bea4ab50e6bc03fb8a2334e2044fddf23db945
Heather <heather@live.ru>
2015-03-20 16:05:20 +0400

    cSIDL_DESKTOPDIRECTORY added to win32

--- src/Win.hs
+++ src/Win.hs
@@ -12,23 +12,11 @@ import Graphics.Win32.GDI.Types
 import Foreign.C.String
 import Foreign.Marshal.Array
 
-foreign import stdcall unsafe "SHGetFolderPathW" -- ccall
-    cSHGetFolderPathW :: HWND -> INT -> HANDLE -> DWORD -> CWString -> IO LONG
-
-maxPath = 260
-
--- Someone just should fix System.Win32.Shell
--- But they don't accept pull requests for ages :(
--- ShlObj.h in MS Platform SDK
-cSIDL_DESKTOPDIRECTORY = 0x0010
-
 getShellFolder :: IO String
 getShellFolder = sHGetFolderPath nullPtr cSIDL_LOCAL_APPDATA nullPtr 0
 
 getDesktopFolder :: IO String
-getDesktopFolder = allocaArray0 maxPath $ \path -> do
-    cSHGetFolderPathW nullHANDLE cSIDL_DESKTOPDIRECTORY nullHANDLE 0 path
-    peekCWString path
+getDesktopFolder = sHGetFolderPath nullPtr cSIDL_DESKTOPDIRECTORY nullPtr 0
 
 getShellRoamingFolder :: IO String
 getShellRoamingFolder = sHGetFolderPath nullPtr cSIDL_APPDATA nullPtr 0
