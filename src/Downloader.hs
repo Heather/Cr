82dd5d3146538b8d0a420f0e0bb55eab43c7a9d1
Heather <heather@live.ru>
2015-04-01 10:45:11 +0400

    HTTP request timeout -> 10 seconds
    TODO: add retry

--- src/Downloader.hs
+++ src/Downloader.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, UnicodeSyntax #-}
+{-# LANGUAGE OverloadedStrings, UnicodeSyntax, ScopedTypeVariables #-}
 
 module Downloader
   ( getLastVersionForPlatform
@@ -21,6 +21,8 @@ import qualified Data.ByteString.Lazy as L
 import qualified Codec.Binary.UTF8.String as S
 
 import Control.Monad
+--import Control.Exception
+--import Control.Concurrent
 import Control.Monad.IO.Class (liftIO)
 
 getLastVersionForPlatform ∷ String → IO String
@@ -29,6 +31,14 @@ getLastVersionForPlatform platform = withSocketsDo
   where url = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/" 
               ++ platform
               ++ "/LAST_CHANGE"
+
+{-
+retryOnTimeout ∷ IO a → IO a
+retryOnTimeout action = catch action $ \ (_ :: HttpException) → do
+    putStrLn "Timed out. Trying again."
+    threadDelay 5000000
+    action
+-}
 
 getChromium ∷ String → String → String → IO()
 getChromium platform version fname = withSocketsDo $ do
@@ -39,10 +49,11 @@ getChromium platform version fname = withSocketsDo $ do
         removeFile fname
     withManager $ \manager → do
         let request = irequest
-             { method = methodGet }
+             { method = methodGet
+             , responseTimeout = Just 10000000 }
         response ← http request manager
         responseBody response C.$$+- sinkFile fname
-  where url = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/" 
-              ++ platform ++ "/" 
-              ++ version ++ "/" 
+  where url = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/"
+              ++ platform ++ "/"
+              ++ version ++ "/"
               ++ fname
